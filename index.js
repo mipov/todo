96b6fa467c61d747ca2e816126ed9affd6360aee
Mikhail Popov <mipo.spb@gmail.com>
2022-05-18 22:37:53 +0300

    Добавил возможность создавать и удалять элементы списка по клику на кнопки

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-let item = document.querySelectorAll('.todo__item');
-let list = document.querySelector('.todo__list')
+ 
 let btnAdd = document.querySelector('.todo__add');
-
+let list = document.querySelector('.todo__list')
 
 btnAdd.addEventListener('click', () => {
-  console.log(newItem)
-  list.insertAdjacentElement('beforebegin', item[0])
+  let newItem = document.createElement('li');
+  newItem.className = 'todo__item'
+  newItem.innerHTML = `<label class="task">
+    <input type="checkbox" class="task__checkbox">
+    <p class="task__text">Сделать хорошо</p>
+    </label>
+    <button class="task__delete"></button>`
+  list.append(newItem)
 })
+
+list.onclick = function(event) {
+  let del = event.target;
+  if (del.className != 'task__delete') return;
+  del.parentElement.remove();
+}
