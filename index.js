6bf95e388e91e08fe34e92f2dbc4aecb3886b191
Mikhail Popov <mipo.spb@gmail.com>
2022-05-19 17:28:54 +0300

    Написал логику, доделал стили. Можно добавлять, удалять и зачеркивать таски

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
  
 let btnAdd = document.querySelector('.todo__add');
-let list = document.querySelector('.todo__list')
+let list = document.querySelector('.todo__list');
+let inp = document.querySelector('.todo__text-input');
 
-btnAdd.addEventListener('click', () => {
+function createTask() {
+  if (inp.value == '') return;
   let newItem = document.createElement('li');
   newItem.className = 'todo__item'
   newItem.innerHTML = `<label class="task">
     <input type="checkbox" class="task__checkbox">
-    <p class="task__text">Сделать хорошо</p>
+    <p class="task__text">${inp.value}</p>
     </label>
     <button class="task__delete"></button>`
-  list.append(newItem)
+  list.appendChild(newItem);
+  return inp.value = '';
+}
+
+btnAdd.addEventListener('click', () => {
+  createTask();
+})
+
+inp.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    createTask();
+  }
 })
 
 list.onclick = function(event) {
